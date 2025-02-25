5321b2f4fcf146bdc76e93e3f003f99ea3f7f41e
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-02-25 09:52:44 +0100

    models.py code fertig

--- programm_database/models.py
+++ programm_database/models.py
@@ -8,16 +8,16 @@ Base = declarative_base()
 
 class User(Base):
   __tablename__ = 'users'
-  id = Column(Integer, primary_key=True)
+  id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
   name = Column(String, nullable=False)
 
   # relationship to UserToMovie class
   movies = relationship("UserToMovie", back_populates="user")
 
 
-class Movies(Base):
+class Movie(Base):
   __tablename__ = 'movies'
-  id = Column(Integer, primary_key=True)
+  id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
   title = Column(String, nullable=False)
   release_date = Column(Integer)
   director_id = Column(Integer, ForeignKey('directors.id'))
@@ -26,25 +26,25 @@ class Movies(Base):
   director = relationship("Director", back_populates="movies")
 
   # relationshiop to UserToMovie class
-  users = relationship("UserToMovie", back_populates="movies")
+  user = relationship("UserToMovie", back_populates="movies")
 
 
 class Director(Base):
   __tablename__ = 'directors'
-  id = Column(Integer, primary_key=True)
+  id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
   first_name = Column(String, nullable=False)
   last_name = Column(String, nullable=False)
 
   # relationship to Movies class
-  movies = relationship("Movies", back_populates="director")
+  movies = relationship("Movie", back_populates="director")
 
 
 class UserToMovie(Base):
   __tablename__ = 'user_to_movie'
-  user_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
-  movie_id = Column(Integer, ForeignKey('movies.id'), primary_key=True)
+  user_id = Column(Integer, ForeignKey('users.id'), primary_key=True, nullable=False)
+  movie_id = Column(Integer, ForeignKey('movies.id'), primary_key=True, nullable=False)
   rating = Column(Numeric)
 
   # relationship to Users and Movies
   user = relationship("User", back_populates="movies")
-  movie = relationship("Movie", back_populates="users")
+  movies = relationship("Movie", back_populates="user")
