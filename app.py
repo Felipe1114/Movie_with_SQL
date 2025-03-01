eb11ed0063e35f9edbf3d9b97e595ab2780d14f7
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-03-01 23:40:43 +0100

    blueprints richtig registert. import buggs behoben

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-from flask import Flask, g, jsonify
+from flask import Flask, g
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from programm_database.models import Base
 from programm_database.database_manager import DatabaseManager
 from programm_backend.movie_routes import movie_bp
 from programm_backend.user_routes import user_bp
+from programm_backend.main_route import main_bp
 from programm_api.movie_service import MovieService
-
-# TODO all routes fertig schreiben!!!
 app = Flask(__name__)
+# TODO ist der movie_bp richtig registiert?
+# TODO routen für die movie seite des users richtig legen
+app.register_blueprint(movie_bp, url_prefix="/user/user_id")
+app.register_blueprint(user_bp, url_prefix="/")
+app.register_blueprint(main_bp)
 
-app.register_blueprint(movie_bp, url_prefix="/api/movies")
-app.register_blueprint(user_bp, url_prefix="/api/users")
-
-DATABASE_URL = "sqlite:///../programm_data/database/movie_database.db"
+DATABASE_URL = "sqlite:///./programm_data/database/movie_database.db"
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -41,9 +42,8 @@ def close_session(exception=None):
         db.close()
 
     api = g.pop('movie_service', None)
-    if api is not None:
-        api.close()
-#
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="127.0.0.1", port=5000)
