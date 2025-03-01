af9950e2cb13beaa68702ee100cf64b1a067a7a1
Felipe1114 <63155769+Felipe1114@users.noreply.github.com>
2025-03-01 11:06:46 +0100

    app.py erweitert

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from programm_database.models import Base
 from programm_database.database_manager import DatabaseManager
 from programm_backend.movie_routes import movie_bp
 from programm_backend.user_routes import user_bp
+from programm_api.movie_service import MovieService
 
-
+# TODO all routes fertig schreiben!!!
 app = Flask(__name__)
 
 app.register_blueprint(movie_bp, url_prefix="/api/movies")
@@ -19,11 +20,19 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # Datenbanktabellen erstellen (nur einmal beim Start)
 Base.metadata.create_all(engine)
 
+API_KEY = "http://www.omdbapi.com/?i=tt3896198&apikey=70e6d1f0"
 # session mit 'g' bereitstellen
-@app.before_request
-def create_session():
+def init_service():
+    # initialisiert die datenbank und ihren zugriff
     g.db = SessionLocal()
     g.db_manager = DatabaseManager(g.db)
+
+    # initialisiert MovieService, damit die API daten in die Datenbank eingesetzt werden können
+    g.movie_service = MovieService(g.db_manager, API_KEY)
+
+@app.before_request
+def create_session():
+    init_service()
 
 @app.teardown_request
 def close_session(exception=None):
@@ -31,6 +40,9 @@ def close_session(exception=None):
     if db is not None:
         db.close()
 
+    api = g.pop('movie_service', None)
+    if api is not None:
+        api.close()
 #
 
 if __name__ == "__main__":
